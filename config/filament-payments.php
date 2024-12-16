23a200e72a23ccc67a3acd671f030c4462cd3216
Muhammad Irfan <mrfansi@outlook.com>
2024-12-16 12:35:24 +0700

    init

---
+++ config/filament-payments.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    "drivers" => [
+        \TomatoPHP\FilamentPayments\Services\Drivers\Cryptomus::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Fawery::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Moyaser::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Payfort::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Paymob::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Paypal::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Paytabs::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Plisio::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\StripeV3::class,
+        \TomatoPHP\FilamentPayments\Services\Drivers\Tap::class,
+    ],
+    "path" => "TomatoPHP\\FilamentPayments\\Services\\Drivers"
+];
